180ecf1ecbd7cff7fe24b9bf19350f8920e00a76
unknown <ayushm185@gmail.com>
2023-11-04 00:14:39 +0530

    for each problem solved

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+  console.log("ayush");
 //  URL
 const url='https://api.weatherapi.com/v1/forecast.json' 
 const key='a0edc6b5af8749b3a8b164009230111'
@@ -12,18 +13,10 @@ const key='a0edc6b5af8749b3a8b164009230111'
  const wind=document.querySelector('#wind')
  const humanity=document.querySelector('#hum')
  const time=document.querySelector('#time')
-
- const last=document.querySelector('.last')
-
- const  childlast=document.querySelector('childlast')
- const sec=document.querySelector('.sec')
-
- const b1=document.querySelector('#lastheading')
-  // b1.style.color = "blue"
-//  EVENT LISTNER
+const sec=document.querySelector('.sec')
 const search=document.querySelector('#search')
-// document.getElementsByClassName('lastheading').style.color="red"
 document.querySelector('#go').addEventListener("click",function(){
+      
       const pure_url=`${url}?key=${key}&q=${val.value}&days=${3}`
       fetch(pure_url).then(function(response){
          return  response.json()
@@ -36,54 +29,14 @@ document.querySelector('#go').addEventListener("click",function(){
            wind.innerHTML=`Wind ${resp1.current.wind_kph}kph`
            humanity.innerHTML=`Humidity ${resp1.current.humidity}%`
            time.innerHTML=`${resp1.location.localtime}`
-          //  console.log(resp1.forecast.forecastday[0].day);
-            // resp1.forecast.forecastday.forEach(function(item){
-            //     const child=document.createElement('div')
-            //     child.classList='childlast'
-            //     //const headig=document.createElement('p')
-            //     //headig.innerHTML='DATE MIN AND MAX TEMP'
-            //     const date=document.createElement('p')
-            //     const mint=document.createElement('p')
-            //     const maxt=document.createElement('p')
-            //     date.innerHTML=item.date;
-            //     mint.innerHTML=`${item.day.mintemp_c} ${'&#176'}C`;	 
-            //     maxt.innerHTML=`${item.day.maxtemp_c}${'&#176'}C`;
-            //     //child.appendChild(headig)
-            //     child.appendChild(date)
-            //     child.appendChild(mint)
-            //     child.appendChild(maxt)
-            //     last.appendChild(child)
+           let i=0;
+            resp1.forecast.forecastday.forEach(function(item){
+              let day=new Date()
+              document.getElementById('last').innerHTML+=`<div class="childlast"><p>${day.getDate()+i}-${day.getMonth()}</p> <p>${item.day.maxtemp_c}${'&#176'}C</p> <p>${item.day.mintemp_c}${'&#176'}C</p></div>`
+              i++
             });
-            // for (let index = 0; index < 3; index++) {
-            //     const childlast=document.createElement('div')
-            //     childlast.id='lastchild'
-            //    const para=document.createElement('p')
-            //     para.innerHTML=resp1.forecast.forecastday[index].date;
-            //    childlast.appendChild(para);
-            //   const mint=document.createElement('p');
-            //   mint.innerHTML=`${resp1.forecast.forecastday[index].day.mintemp_c}C`
-            //   childlast.appendChild(mint)
-            //   const maxt=document.createElement('p')
-            //    maxt.innerHTML=`${resp1.forecast.forecastday[index].day.maxtemp_c}C`
-            //   childlast.appendChild(maxt)
-            //    console.log(childlast);
-            //    last.appendChild(childlast) ;  
-            // console.log("ayush");
-            // }
-            /*
-            const childlast=document.createElement('div')
-            childlast.id='lastchild'
-            const para=document.createElement('p')
-            para.innerHTML=item.date;
-            childlast.appendChild(para);
-            const mint=document.createElement('p');
-            mint.innerHTML=`${item.day.mintemp_c}C`
-            childlast.appendChild(mint)
-            const maxt=document.createElement('p')
-            maxt.innerHTML=`${item.day.maxtemp_c}C`
-            childlast.appendChild(maxt)
-            console.log(childlast);
-            last.appendChild(childlast) ;  */  
-      }).catch(function(error){
-        console.log(error);
+          }).catch(function(e){
+            console.log(e);
+          })
+       document.getElementById('last').innerHTML="" 
       })
