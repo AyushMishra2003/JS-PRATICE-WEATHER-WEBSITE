d32ed31b5482f537efee21f72ded5e7cee0f37ca
unknown <ayushm185@gmail.com>
2023-11-03 17:38:06 +0530

    css style problem solved

--- script.js
+++ script.js
@@ -18,21 +18,42 @@ const key='a0edc6b5af8749b3a8b164009230111'
  const  childlast=document.querySelector('childlast')
  const sec=document.querySelector('.sec')
 
+ const b1=document.querySelector('#lastheading')
+  // b1.style.color = "blue"
 //  EVENT LISTNER
 const search=document.querySelector('#search')
+// document.getElementsByClassName('lastheading').style.color="red"
 document.querySelector('#go').addEventListener("click",function(){
       const pure_url=`${url}?key=${key}&q=${val.value}&days=${3}`
       fetch(pure_url).then(function(response){
          return  response.json()
       }).then(function(resp1){
         console.log(resp1);
-           temp.innerHTML=`${resp1.current.temp_c}C|`
-           far.innerHTML=`${resp1.current.temp_f}F`
+           temp.innerHTML=`${resp1.current.temp_c}${'&#176'}C`
+           far.innerHTML=`${resp1.current.temp_f}${'&#176'}F`
            logo.src=resp1.current.condition.icon
            locati.innerHTML=`${resp1.location.name}|${resp1.location.region}`
            wind.innerHTML=`Wind ${resp1.current.wind_kph}kph`
            humanity.innerHTML=`Humidity ${resp1.current.humidity}%`
            time.innerHTML=`${resp1.location.localtime}`
+          //  console.log(resp1.forecast.forecastday[0].day);
+            resp1.forecast.forecastday.forEach(function(item){
+                const child=document.createElement('div')
+                child.classList='childlast'
+                //const headig=document.createElement('p')
+                //headig.innerHTML='DATE MIN AND MAX TEMP'
+                const date=document.createElement('p')
+                const mint=document.createElement('p')
+                const maxt=document.createElement('p')
+                date.innerHTML=item.date;
+                mint.innerHTML=`${item.day.mintemp_c} ${'&#176'}C`;	 
+                maxt.innerHTML=`${item.day.maxtemp_c}${'&#176'}C`;
+                //child.appendChild(headig)
+                child.appendChild(date)
+                child.appendChild(mint)
+                child.appendChild(maxt)
+                last.appendChild(child)
+            });
             // for (let index = 0; index < 3; index++) {
             //     const childlast=document.createElement('div')
             //     childlast.id='lastchild'
