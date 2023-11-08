6571ccfd626220f372df1c367d277066c744edab
unknown <ayushm185@gmail.com>
2023-11-08 19:54:23 +0530

    daily temperature problem solved

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 
-//  URL
 const url='https://api.weatherapi.com/v1/forecast.json' 
 const key='a0edc6b5af8749b3a8b164009230111'
  const val=document.querySelector('#searchval')
-
-//  //  EXRESS 
-//  const logo=document.querySelector('#logo')
-//  const temp=document.querySelector('#tempr')
-//  const far=document.querySelector('#far')
-//  const locati=document.querySelector('#location')
-//  const wind=document.querySelector('#wind')
-//  const humanity=document.querySelector('#hum')
-//  const time=document.querySelector('#time')
-// const sec=document.querySelector('.sec')
-// const search=document.querySelector('#search')
 document.querySelector('#search').addEventListener("click",function(){
-      console.log("Helllo brother");
       const pure_url=`${url}?key=${key}&q=${val.value}&days=${3}`
       fetch(pure_url).then(function(response){
          return  response.json()
@@ -32,20 +19,28 @@ document.querySelector('#search').addEventListener("click",function(){
             resp1.forecast.forecastday.forEach(function(item){
               document.getElementById('last').innerHTML+=`<div class="childlast"><p>${item.date}</p>   <img  src="${item.day.condition.icon}" alt=""> <p>${item.day.maxtemp_c}${'&#176'}C</p> <p>${item.day.mintemp_c}${'&#176'}C</p></div>`
             });
-           
             const result=resp1.forecast.forecastday[0].hour
-            console.log(result);
+            const day=new Date()
+            const time=`${day.getHours()}:${"00"}`
             const hr=result.filter(function(item){
-                console.log(item);
-                // return (item.time>`${"2023-11-08 10:00"}`)
-                return (item.time>`${new Date().getDate}``${new Date().getT}`)
+                let t=`${item.time[11]}${item.time[12]}:${"00"}`
+                return (t>=time);  
             })
-            console.log("hour is ",hr);
-            hr.forEach(function(hour){
+            const result1=resp1.forecast.forecastday[1].hour
+            const hr1=result1.filter(function(item){
+              let t=`${item.time[11]}${item.time[12]}:${"00"}`
+              return (t<=time);  
+          })
+          hr.forEach(function(hour){
                document.getElementById('dailytemp').innerHTML+=`<div class="daily"><p>${hour.time}</p> <img src="${hour.condition.icon}" <p>${hour.feelslike_c}${'$176'}</p>
                </div>`
             })
-          });
+          // });
+          hr1.forEach(function(hour){
+            document.getElementById('dailytemp').innerHTML+=`<div class="daily"><p>${hour.time}</p> <img src="${hour.condition.icon}" <p>${hour.feelslike_c}${'$176'}</p>
+            </div>`
+         })
+        });
           document.getElementById('last').innerHTML="" 
           val.value=""
 
